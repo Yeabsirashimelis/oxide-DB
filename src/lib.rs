437bbf27200e6c1f0c3c3514f8dc9cc571f0d7fb
yeabsira <shimelisyeabsiragithub@gmail.com>
2025-10-23 09:43:36 -0700

    declaring the type aliases we used in data operations

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,10 @@
+/*
+   ByteStr is to &str what ByteString is to Vec<u8>.
 
+   This code processes lots of Vec<u8> data. Because that is used in the same way as String tends to be used,
+    ByteString is a useful alias.
+*/
+
+type ByteString = Vec<u8>;
+
+type ByteStr = [u8];
