3aced0ae28fb0904d1ab9f95c89be3e33da9bf99
yeabsira <shimelisyeabsiragithub@gmail.com>
2025-10-24 00:29:32 -0700

    Implement OxideDB core structure and file loading logic

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,65 @@
     ByteString is a useful alias.
 */
 
+use std::{
+    collections::HashMap,
+    fs::{File, OpenOptions},
+    io::{self, BufReader, Seek, SeekFrom},
+    path::Path,
+};
+
+use serde::{Deserialize, Serialize};
+
 type ByteString = Vec<u8>;
 
 type ByteStr = [u8];
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct OxideDB {
+    f: File,
+    pub index: HashMap<ByteString, u64>,
+}
+
+impl OxideDB {
+    pub fn open(path: &Path) -> io::Result<Self> {
+        let f = OpenOptions::new()
+            .read(true)
+            .write(true)
+            .create(true)
+            .append(true)
+            .open(path)?;
+
+        let index = HashMap::new();
+        Ok(OxideDB { f, index })
+    }
+
+    // OxideDB::load() - populates the index of the OxideDB struct, mapping keys to file positions
+    pub fn load(&mut self) -> io::Result<()> {
+        let mut f = BufReader::new(&mut self.f);
+
+        loop {
+            // File::seek() returns the number of bytes from the start of the file. This becomes the value of the index
+            let position = f.seek(SeekFrom::Current(0))?;
+
+            // OxideDB::process_record - reads a record in the file at its current position
+            let maybe_kv = OxideDB::process_record(&mut f);
+
+            let kv = match maybe_kv {
+                Ok(kv) => kv,
+                Err(err) => match err.kind() {
+                    /*
+                    File operations in Rust might return an error of type std::io::ErrorKind::UnexpectedEoF
+
+                    */
+                    io::ErrorKind::UnexpectedEof => {
+                        break;
+                    }
+                    _ => return Err(err),
+                },
+            };
+
+            self.index.insert(kv.key, position);
+        }
+        Ok(())
+    }
+}
